ff10c03b31287fea50aa3976fce2b05372b60cfb
Gamzakov <justgamzakov@gmail.com>
2018-03-23 15:24:53 +0300

    Initial commit

---
+++ Kontur.ImageTransformer/Attributes/FilterProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kontur.ImageTransformer.Attributes
+{
+    public class FilterProcessor : Attribute
+    {
+        public string Filter { get; }
+
+        public FilterProcessor(string filter)
+        {
+            Filter = filter;
+        }
+    }
+}
